6ce6cfdd332556d4c99eb2178a5bda12d8680b6c
Steffen Wolf <steffen.wolf@iwr.uni-heidelberg.de>
2018-08-29 20:14:34 +0200

    add non local mutex ws segmentation

--- embeddingutils/segmentation.py
+++ embeddingutils/segmentation.py
@@ -24,7 +24,7 @@ def mws_segmentation(embedding, offsets='default-3D', affinity_measure=logistic_
         repulsive_strides = (1,) * (n_img_dims - 2) + (8, 8)
 
     # actually not needed, huge offsets are fine
-    #for off in offsets:
+    # for off in offsets:
     #    assert all(abs(o) < s for o, s in zip(off, emb.shape[-len(off):])), \
     #        f'offset {off} is to big for image of shape {img_shape}'
     if affinity_measure is not None:
@@ -61,6 +61,59 @@ def mws_segmentation(embedding, offsets='default-3D', affinity_measure=logistic_
         return torch.from_numpy(result), affinities
     else:
         return torch.from_numpy(result)
+
+
+def nonlocal_mws_segmentation(att_aff, offsets,
+                              mutex_edges, mutex_edge_weights):
+
+    # determine the strides of the image
+    shape = att_aff.shape[1:]
+    array_stride = [0] * len(shape)
+    current_stride = 1
+    for i in reversed(range(len(shape))):
+        array_stride[i] = current_stride
+        current_stride *= shape[i]
+
+    # generate attractive edge list
+    attractive_edge_weights = []
+    attractive_edges = []
+
+    # check which edges leave image
+    valid_edges = np.ones(att_aff.shape, dtype=bool)
+    ndim = len(offsets[0])
+    for i, offset in enumerate(offsets):
+        for j, o in enumerate(offset):
+            # invalid_slice = (i, ) + i * (slice(None), ) + slice(o)
+            inv_slice = slice(0, -o) if o < 0 else slice(shape[j] - o, shape[j])
+            invalid_slice = (i, ) + tuple(slice(None) if j != d else inv_slice
+                                          for d in range(ndim))
+            valid_edges[invalid_slice] = 0
+
+    # generate list of valid attractive edges by (u, v)
+    source_points = np.arange(np.product(shape)).reshape(shape)
+    for idx, offset in enumerate(offsets):
+
+        # compute stride
+        stride = 0
+        for i in range(len(offset)):
+            stride += offset[i] * array_stride[i]
+
+        target_points = source_points + stride
+        attractive_edges.append(np.stack([source_points[valid_edges[idx]].ravel(),
+                                          target_points[valid_edges[idx]].ravel()], axis=1))
+        attractive_edge_weights.append(att_aff[idx][[valid_edges[idx]]].ravel())
+
+    attractive_edges = np.concatenate(attractive_edges, axis=0)
+    attractive_edge_weights = np.concatenate(attractive_edge_weights, axis=0)
+
+    number_of_labels = np.product(shape)
+    node_labels = compute_mws_clustering(number_of_labels,
+                                         attractive_edges.astype('uint64'),
+                                         mutex_edges.astype('uint64'),
+                                         (1 - attractive_edge_weights).astype('float'),
+                                         (1 - mutex_edge_weights).astype('float'))
+
+    return node_labels.reshape(shape)
 
 
 def hdbscan_segmentation(embedding, n_img_dims=None, coord_scales=None,
@@ -105,34 +158,50 @@ def hdbscan_segmentation(embedding, n_img_dims=None, coord_scales=None,
 
 
 if __name__ == '__main__':
-    from embeddingutils.affinities import normalized_cosine_similarity, logistic_similarity
-    import matplotlib.pyplot as plt
-    emb = torch.ones((2, 11, 11)).float()
-    emb[0] = -1
-    emb[0, :5, :3] = 1
-    emb[1, :5, :3] = -1
-    emb[1, 7:9, 4:] = -2
-    emb[0, 4:, 7:9] = 2
-    emb = emb + torch.randn(emb.shape)/5
-    emb = emb[:, None]
-    print('embedding shape:', emb.shape)
 
-    for e in emb:
-        plt.imshow(e[0])
-        plt.show()
+    import h5py
+    with h5py.File("/export/home/swolf/local/src/data/skeleton_affinities.h5") as skaff:
+        att_aff = skaff['attractive'].value
+        u = skaff['U'].value
+        v = skaff['V'].value
+        mutex_edges = np.stack([u, v], axis=1)
+        mutex_edge_weights = skaff['W'].value
+        shape = att_aff.shape[1:]
 
-    #for aff in embedding_to_affinities(emb, offsets='default-2D', affinity_measure=euclidean_similarity):
-    #    print(aff.shape)
-    #    print(torch.max(aff), torch.min(aff))
+    offsets = [[-1, 0], [0, -1]]
+    segmentation = nonlocal_mws_segmentation(att_aff, offsets,
+                                             mutex_edges, mutex_edge_weights)
 
-    offsets = ((0, 1), (1, 0), (0, 1), (1, 0))
-    segs = (mws_segmentation(emb, offsets=offsets, affinity_measure=logistic_similarity,
-                               ATT_C=2, repulsive_strides=(1, 1)))
-    for s in segs:
-        plt.imshow(s)
-        plt.show()
+    # import pdb; pdb.set_trace()
 
-    segs = hdbscan_segmentation(emb, metric='euclidean', min_cluster_size=8, n_img_dims=2, coord_scales=(.1, .1))
-    for s in segs:
-        plt.imshow(s)
-        plt.show()
+    # from embeddingutils.affinities import normalized_cosine_similarity, logistic_similarity
+    # import matplotlib.pyplot as plt
+    # emb = torch.ones((2, 11, 11)).float()
+    # emb[0] = -1
+    # emb[0, :5, :3] = 1
+    # emb[1, :5, :3] = -1
+    # emb[1, 7:9, 4:] = -2
+    # emb[0, 4:, 7:9] = 2
+    # emb = emb + torch.randn(emb.shape) / 5
+    # emb = emb[:, None]
+    # print('embedding shape:', emb.shape)
+
+    # for e in emb:
+    #     plt.imshow(e[0])
+    #     plt.show()
+
+    # # for aff in embedding_to_affinities(emb, offsets='default-2D', affinity_measure=euclidean_similarity):
+    # #    print(aff.shape)
+    # #    print(torch.max(aff), torch.min(aff))
+
+    # offsets = ((0, 1), (1, 0), (0, 1), (1, 0))
+    # segs = (mws_segmentation(emb, offsets=offsets, affinity_measure=logistic_similarity,
+    #                          ATT_C=2, repulsive_strides=(1, 1)))
+    # for s in segs:
+    #     plt.imshow(s)
+    #     plt.show()
+
+    # segs = hdbscan_segmentation(emb, metric='euclidean', min_cluster_size=8, n_img_dims=2, coord_scales=(.1, .1))
+    # for s in segs:
+    #     plt.imshow(s)
+    #     plt.show()
